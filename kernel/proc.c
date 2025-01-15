5136c28ccf032a3d29c654d88c095ae71aaaabf8
RAMBUS007 <1216804161@qq.com>
2025-01-15 16:08:51 +0800

    first version

--- kernel/proc.c
+++ kernel/proc.c
@@ -268,6 +268,9 @@ fork(void)
   // copy saved user registers.
   *(np->tf) = *(p->tf);
 
+  // copy traced syscalls
+  np->mask = p->mask;
+
   // Cause fork to return 0 in the child.
   np->tf->a0 = 0;
 
@@ -680,4 +683,16 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+uint64
+nproc() {
+  uint64 n = 0;
+
+  for(struct proc *p = proc; p < &proc[NPROC]; p++){
+    acquire(&p->lock);
+    if(p->state != UNUSED) ++n;
+    release(&p->lock);
+  }
+  return n;
 }
--- kernel/syscall.h
+++ kernel/syscall.h
@@ -20,6 +20,8 @@
 #define SYS_link   19
 #define SYS_mkdir  20
 #define SYS_close  21
+#define SYS_trace  22
+#define SYS_sysinfo 23
 
 // System calls for labs
 #define SYS_ntas   22
